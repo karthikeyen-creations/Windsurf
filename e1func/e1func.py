48e6661577be4707fcd102af3170e6e0eda1f539
karthikeyen-creations <55886253+karthikeyen-creations@users.noreply.github.com>
2024-12-16 16:12:34 +0530

    s4

--- e1func/e1func.py
+++ e1func/e1func.py
@@ -56,11 +56,12 @@ st.markdown("""
     </style>
 """, unsafe_allow_html=True)
 
-def get_directories_from_csv(csv_content):
+def get_directories_from_csv(filepath):
     directories = []
-    csv_file = csv_content.strip().split('\n')
-    for row in csv_file:
-        directories.append(row.strip())
+    with open(filepath, 'r') as file:
+        csv_file = file.read().strip().split('\n')
+        for row in csv_file:
+            directories.append(row.strip())
     return directories
 
 def list_files(directories):
@@ -138,17 +139,32 @@ def update_temp_list(directory, index, new_sel):
             break
     st.session_state.temp_list = st.session_state.temp_list
 
+def list_directories(directory):
+    all_directories = []
+    for root, dirs, _ in os.walk(directory):
+        for d in dirs:
+            all_directories.append(os.path.relpath(os.path.join(root, d), directory))
+    return all_directories
+
 st.title("Directory File Lister")
 
-tab1, tab2, tab3 = st.tabs(["List Files", "Manage Selections", "Selected Files"])
+tab1, tab2, tab3, tab4 = st.tabs(["List Directories", "List Files", "Manage Selections", "Generate Combined File"])
 
 with tab1:
-    csv_content = """SQLcheck\\columns
-    SQLcheck\\sample\\pg"""
+    base_directory = st.text_input("Base Directory", value=".")
+    if st.button("List Directories"):
+        directories = list_directories(base_directory)
+        if directories:
+            st.write("Directories:")
+            for directory in directories:
+                st.write(os.path.join(base_directory, directory))
+        else:
+            st.write("No directories found.")
+
+with tab2:
+    csv_content = get_directories_from_csv('e1func/workfiles/dirlst.csv')
     
-    directories = get_directories_from_csv(csv_content)
-    
-    edited_directories = st.text_area("Edit Directories", value="\n".join(directories), height=100)
+    edited_directories = st.text_area("Edit Directories", value="\n".join(csv_content), height=100)
     
     if st.button("List Files"):
         directories = get_directories_from_csv(edited_directories)
@@ -168,7 +184,7 @@ with tab1:
         timestamp = datetime.now().strftime("%Y%m%d%H%M%S")
         save_file_list(file_list, f'e1func/workfiles/filst{timestamp}.csv')
 
-with tab2:
+with tab3:
     if 'temp_list' not in st.session_state:
         sel_values, latest_file = get_latest_file_sel_values('e1func/workfiles')
         directories = {}
@@ -212,7 +228,7 @@ with tab2:
         st.success("Selections updated successfully!")
         st.rerun()  # Trigger a rerun
 
-with tab3:
+with tab4:
     sel_values, latest_file = get_latest_file_sel_values('e1func/workfiles')
     st.subheader(f"Selected Files from {latest_file}")
     
